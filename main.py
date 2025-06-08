482dd6a7eb752213c4f9a9f0f894cac5e660bef7
Chereshnia <vesmih2023@gmail.com>
2025-06-08 13:59:52 +0300

    use pool

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from asyncio import gather
+
 import asyncpg
 import asyncio
 from random import sample, randint
@@ -53,6 +55,21 @@ INSERT INTO product_size VALUES(2, 'Medium');
 INSERT INTO product_size VALUES(3, 'Large')
 """
 
+product_query = """
+SELECT
+p.product_id,
+p.product_name,
+p.brand_id,
+s.sku_id,
+pc.product_color_name,
+ps.product_size_name
+FROM product as p
+JOIN sku as s on s.product_id = p.product_id
+JOIN product_color as pc on pc.product_color_id = s.product_color_id
+JOIN product_size as ps on ps.product_size_id = s.product_size_id
+WHERE p.product_id = 100
+"""
+
 def load_common_words() -> List[str]:
     with open('common_words.txt') as common_words:
         result = common_words.readlines()
@@ -93,31 +110,29 @@ def gen_skus(product_id_start: int,
 
     return skus
 
+async def query_product(pool):
+    async with pool.acquire() as connection:
+        return await connection.fetchrow(product_query)
+
+async def query_products_synchronously(pool, queries):
+    return [await query_product(pool) for _ in range(queries)]
+
+async def query_products_concurrently(pool, queries):
+    queries = [query_product(pool) for _ in range(queries)]
+    return await asyncio.gather(*queries)
 
 async def main():
-    common_words = load_common_words()
-    connection = await asyncpg.connect(host='localhost',
-                                       port=5432,
-                                       user='postgres',
-                                       database='postgres',
-                                       password='postgres')
+    async with asyncpg.create_pool(host='localhost',
+                                   port=5432,
+                                   user='postgres',
+                                   database='postgres',
+                                   password='postgres',
+                                   min_size=6,
+                                   max_size=6) as pool:
 
-    product_tuples = gen_products(common_words,
-                                  1,
-                                  100,
-                                  1000)
+        await query_products_concurrently(pool, 1000)
+        await query_products_synchronously(pool, 1000)
 
-    await connection.executemany("INSERT INTO product VALUES(DEFAULT, $1, $2)"
-                                 ""
-                                 "", product_tuples)
-
-    sku_tuples = gen_skus(product_id_start=1,
-                          product_id_end=1000,
-                          skus_to_create=100000)
-
-    await connection.executemany("INSERT INTO sku VALUES(DEFAULT, $1, $2, $3)", sku_tuples)
-
-    await connection.close()
 
 
 asyncio.run(main())
