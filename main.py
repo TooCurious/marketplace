88ed4399f33a98e45b993e7485b175d7c6d9299b
Chereshnia <vesmih2023@gmail.com>
2025-06-13 06:00:30 +0300

    use gather

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 
 import asyncpg
 import asyncio
+import aiohttp
 from random import sample, randint
 from typing import List, Tuple, Union
 
@@ -11,29 +12,17 @@ import selectors
 from selectors import SelectorKey
 from asyncio import AbstractEventLoop
 
-async def echo(connection: socket, loop: AbstractEventLoop):
-    while data := await loop.sock_recv(connection, 1024):
-        print(f'Данные получены {data}')
-        await loop.sock_sendall(connection, data)
-
-async def listen_for_connections(server_socket: socket, loop: AbstractEventLoop):
-    while True:
-        connection, address = await loop.sock_accept(server_socket)
-        connection.setblocking(False)
-        print(f'Получен запрос на подключение {address}')
-        asyncio.create_task(echo(connection, loop))
+from aiohttp import ClientSession
+from util import async_timed, fetch_status
 
 
+@async_timed.async_timed()
 async def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-
-    server_address = ('127.0.0.1', 8000)
-    server_socket.bind(server_address)
-    server_socket.listen()
-
-    await listen_for_connections(server_socket, asyncio.get_event_loop())
+    async with aiohttp.ClientSession() as session:
+        urls = ["https://www.exaple.com" for _ in range(1000)]
+        request = [fetch_status.fetch_status(session, url) for url in urls]
+        status_codes = await asyncio.gather(*request)
+        print(status_codes)
 
 
 asyncio.run(main())
